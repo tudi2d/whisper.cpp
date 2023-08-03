7b1d90d0c6c976b530a9da8e3d2ba97d0e7babab
Philipp Hugenroth <philipph@spotify.com>
2023-08-03 17:26:52 +0200

    change print format

--- examples/whisper.wasm/emscripten.cpp
+++ examples/whisper.wasm/emscripten.cpp
@@ -66,7 +66,7 @@ EMSCRIPTEN_BINDINGS(whisper) {
             g_contexts[index] = nullptr;
         }
     }));
-    emscripten::function("full_default", emscripten::optional_override([](size_t index, const emscripten::val & audio, const std::string & lang, int nthreads, bool translate, int max_len) {
+    emscripten::function("full_default", emscripten::optional_override([](size_t index, const emscripten::val & audio, const std::string & lang, int nthreads, bool translate, int max_len, bool tdrz) {
         if (g_worker.joinable()) {
             g_worker.join();
         }
@@ -86,13 +86,14 @@ EMSCRIPTEN_BINDINGS(whisper) {
         params.print_realtime   = false;
         params.print_timestamps = true;
         params.token_timestamps = true; // required for `max_len`
-        params.max_len          = 1;
+        params.max_len          = max_len;
         params.print_special    = false;
         params.translate        = translate;
         params.language         = whisper_is_multilingual(g_contexts[index]) ? lang.c_str() : "en";
         params.n_threads        = std::min(nthreads, std::min(16, mpow2(std::thread::hardware_concurrency())));
         params.offset_ms        = 0;
         params.split_on_word    = true;
+        params.tdrz_enable      = tdrz;
 
         // this callback is called on each new segment
         if (!params.print_realtime) {
@@ -106,24 +107,17 @@ EMSCRIPTEN_BINDINGS(whisper) {
                 // print the last n_new segments
                 const int s0 = n_segments - n_new;
 
-                if (s0 == 0) {
-                    printf("\n");
-                }
-
                 for (int i = s0; i < n_segments; i++) {
                     t0 = whisper_full_get_segment_t0(ctx, i);
                     t1 = whisper_full_get_segment_t1(ctx, i);
-                    printf("[%s --> %s]  ", to_timestamp(t0).c_str(), to_timestamp(t1).c_str());
-                    printf("\n");
+                    printf("TIME##%lld##%lld\n", t0, t1);
                 
                     for (int j = 0; j < whisper_full_n_tokens(ctx, i); ++j) {
                         const char * text = whisper_full_get_token_text(ctx, i, j);
                         const float  p     = whisper_full_get_token_p(ctx, i, j);
 
-                        printf("text: %s; token.p: %f\n", text, p);
+                        printf("TEXT##%s##%f\n", text, p);
                     }
-
-                    printf("\n");
                 }
             };
         }
@@ -152,8 +146,6 @@ EMSCRIPTEN_BINDINGS(whisper) {
                     params.translate ? "translate" : "transcribe",
                     params.max_len
                     );
-
-            printf("\n");
         }
 
         // run the worker
@@ -161,26 +153,6 @@ EMSCRIPTEN_BINDINGS(whisper) {
             g_worker = std::thread([index, params, pcmf32 = std::move(pcmf32)]() {
                 whisper_reset_timings(g_contexts[index]);
                 whisper_full(g_contexts[index], params, pcmf32.data(), pcmf32.size());
-
-                const int n_segments = whisper_full_n_segments(g_contexts[index]);
-                printf("n_segments: %d\n", n_segments);
-                for (int i = 0; i < n_segments; ++i) {
-                    const char * text = whisper_full_get_segment_text(g_contexts[index], i);
-
-                    int prob_n = 0;
-                    float prob = 0.0f;
-
-                    // n_tokens = 1 for max_len = 1
-                    const int n_tokens = whisper_full_n_tokens(g_contexts[index], i);
-                    for (int j = 0; j < n_tokens; ++j) {
-                        const auto token = whisper_full_get_token_data(g_contexts[index], i, j);
-                        prob += token.p;
-                        ++prob_n;
-                    }
-
-                    printf("text: %s; token.p: %f\n", text, prob/prob_n);
-                }
-                
                 whisper_print_timings(g_contexts[index]);
             });
         }
